d5c24dd76d1b58b9745e57aa2d748f65dce2bd3f
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2025-03-09 13:23:14 +0200

    Add search functionality to HomeScreen with TextField and clear button

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:store_app/consts/global_colors.dart';
 
 import '../widget/appbar_icons.dart';
 
@@ -11,27 +12,79 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late TextEditingController _searchController;
+
+  @override
+  void initState() {
+    _searchController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
       },
-      child:  Scaffold(
+      child: Scaffold(
         appBar: AppBar(
           title: const Text("Home"),
           leading: AppBarIcons(
             function: () {},
             icon: IconlyBold.category,
           ),
-          actions:  [
+          actions: [
             AppBarIcons(
               function: () {},
               icon: IconlyBold.user3,
             ),
           ],
         ),
-        body: const Center(child: Text("Welcome to this course")),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          child: Column(
+            children: [
+              TextField(
+                controller: _searchController,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      _searchController.clear();
+                    },
+                    icon: Icon(
+                      IconlyLight.search,
+                      color: lightIconsColor,
+                    ),
+                  ),
+                  hintText: "Search",
+                  filled: true,
+                  fillColor: Theme.of(context).cardColor,
+                  prefixIcon: const Icon(IconlyBold.search),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).cardColor,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(
+                      color: Theme.of(context).colorScheme.secondary,
+                      width: 1,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
