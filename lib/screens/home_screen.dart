d70061b68fe9dc7ed3726d7fa6b82f08dcf2f1d5
Mahmoud Ahmed <mahmoudabuelazem2467@gmail.com>
2025-03-09 14:22:57 +0200

    Add FeedsWidget and integrate carousel slider for SaleWidget in HomeScreen; update dependencies in pubspec.yaml

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,10 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
 import 'package:store_app/consts/global_colors.dart';
 
 import '../widget/appbar_icons.dart';
+import '../widget/feeds_widget.dart';
 import '../widget/sale_widget.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,6 +16,8 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   late TextEditingController _searchController;
+  int _current = 0;
+  final CarouselSliderController _controller = CarouselSliderController();
 
   @override
   void initState() {
@@ -24,11 +28,14 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void dispose() {
     _searchController.dispose();
+
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
@@ -82,10 +89,76 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
               ),
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
-              const SaleWidget(),
+              SizedBox(
+                height: size.height * 0.25,
+                child: Stack(
+                  alignment: AlignmentDirectional.bottomCenter,
+                  children: [
+                    CarouselSlider(
+                      carouselController: _controller,
+                      items: [0, 1, 2].map((i) {
+                        return Builder(builder: (BuildContext context) {
+                          return SaleWidget(
+                            i: i,
+                          );
+                        });
+                      }).toList(),
+                      options: CarouselOptions(
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            _current = index;
+                          });
+                        },
+                        height: size.height * 0.25,
+                        autoPlay: true,
+                        enlargeCenterPage: true,
+                      ),
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [0, 1, 2].map((i) {
+                        return GestureDetector(
+                          onTap: () => _controller.animateToPage(i),
+                          child: AnimatedContainer(
+                            duration: const Duration(milliseconds: 300),
+                            width: 6.0,
+                            height: 6.0,
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 2.0),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: _current == i
+                                  ? Theme.of(context).colorScheme.secondary
+                                  : Theme.of(context).cardColor,
+                            ),
+                          ),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    const Text(
+                      "Latest Products",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const Spacer(),
+                    AppBarIcons(function: () {}, icon: IconlyBold.arrowRight2),
+                  ],
+                ),
+              ),
+              FeedsWidget(),
             ],
           ),
         ),
